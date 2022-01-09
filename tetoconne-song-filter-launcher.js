737f560e3fd1e26103cffff2e40d5f3590afc241
SHIMMAND <58391224+shimmand@users.noreply.github.com>
2022-01-09 19:57:01 +0900

    Update tetoconne-song-filter-launcher.js

--- tetoconne-song-filter-launcher.js
+++ tetoconne-song-filter-launcher.js
@@ -1,1 +1,1 @@
-javascript:(function(){import { main } from 'https://shimmand.github.io/rhythm-game-utilities/tetoconne-song-filter.js'; main();})();
+javascript:(function(){const path = 'https://shimmand.github.io/rhythm-game-utilities/tetoconne-song-filter.js'; const element = document.createElement("script"); element.src = path; document.body.appendChild(element);})();
